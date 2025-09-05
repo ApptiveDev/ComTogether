bae1262ab7c93196f54ea408cc1537fb4a52ba71
s202455374 <yimnabin20051022@gmail.com>
2025-09-05 11:46:38 +0900

    chore: 백엔드 프로젝트 초기 설정
    - build.gradle 설정 추가
    - Spring Boot 기본 의존성 추가 (Web, JPA, Validation, Lombok, MySQL)
    - 임시로 로컬 DB와 연결 설정

---
+++ backend/src/main/java/com/cmg/comtogether/ComtogetherApplication.java
@@ -0,0 +1,13 @@
+package com.cmg.comtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComtogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComtogetherApplication.class, args);
+	}
+
+}
