b0b48f4a6e5ad1dc2b97bf6cbe484d8c89f7d9b2
s202455374 <yimnabin20051022@gmail.com>
2025-11-11 14:26:30 +0900

    feat: 관리자용 로그인 기능 추가
    - 이메일, 비밀번호 입력으로 로그인
    - ADMIN 권한 추가 및 jwt 권한 세부 설정

--- backend/src/main/java/com/cmg/comtogether/common/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/cmg/comtogether/common/security/CustomUserDetailsService.java
@@ -16,8 +16,14 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-        User user = userRepository.findById(Long.valueOf(userId))
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(ErrorCode.USER_NOT_FOUND.getMessage()));
+        return new CustomUserDetails(user);
+    }
+
+    public UserDetails loadUserById(Long userId) {
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UsernameNotFoundException(ErrorCode.USER_NOT_FOUND.getMessage()));
         return new CustomUserDetails(user);
     }
